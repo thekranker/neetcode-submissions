28a269f3ae84b442ab5f4ec4c421baca346d0486
Karan Kathuria <141097537+thekranker@users.noreply.github.com>
2026-08-15 17:54:34 -0700

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        
+        cache = {}
+
+        def dp(index: int):
+            if index not in range(len(nums)):
+                return 0
+            if index in cache:
+                return cache[index]
+
+            result = max(nums[index] + dp(index+2), dp(index+1))
+            cache[index] = result
+
+            return result
+
+        return dp(0)
